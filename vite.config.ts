1ad7ac0484ae80e6d9b19d5746dff73ec6d65427
DucCuong1905 <72561292+DucCuong1905@users.noreply.github.com>
2026-05-28 09:30:27 +0700

    build: disable compressed size reporting
    Reduce build time by disabling the generation of compressed size reports in Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({mode}) => {
       },
     },
     build: {
+      reportCompressedSize: false,
       chunkSizeWarningLimit: 1000,
       rollupOptions: {
         output: {
